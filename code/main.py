632fee8d477ff88fe293070c928d39c9650ec046
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-02-18 23:02:23 +0100

    basic build, import map

---
+++ code/main.py
@@ -0,0 +1,45 @@
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Platformer')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+        
+        # load game
+        self.setup()
+        
+    def setup(self):
+        tmx_map = load_pygame(join('data', 'maps', 'world.tmx'))
+        
+        for x, y, image in tmx_map.get_layer_by_name('Main').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites, self.collision_sprites))
+        
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(FRAMERATE) / 1000
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    
+            # update
+            self.all_sprites.update(dt)
+            
+            # draw
+            self.display_surface.fill(BG_COLOR)
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+            
+        pygame.quit()
+    
+if __name__ == '__main__':
+    game = Game()
+    game.run()
